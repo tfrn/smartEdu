0f9514a89859db8e778289b1f788e57c36a61889
Tolga Fermanci <tfrn@live.fr>
2023-01-13 15:10:17 +0300

    second commit

--- controllers/pageControllers.js
+++ controllers/pageControllers.js
@@ -1,6 +1,7 @@
 const nodemailer = require("nodemailer");
 const Course = require("../models/Course");
 const User = require("../models/User");
+const dotenv = require('dotenv').config();
 
 exports.getIndex = async (req, res) => {
   try {
@@ -56,29 +57,29 @@ exports.sendMessage = async (req, res) => {
   `
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     secure: true, // true for 465, false for other ports
     auth: {
-      user: "nrfttfrn@gmail.com", // gmail account
-      pass: "iwqacryzzpzbjzmz", // gmail password
+      user: process.env.EMAIL_ADRESS, // gmail account
+      pass: process.env.EMAIL_PASSWORD // gmail password
     },
   });
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
-    from: '"smartEdu contact form" <nrfttfrn@gmail.com>', // sender address
-    to: "tfrn@live.fr", // list of receivers
+    from: `"smartEdu contact form" ${process.env.EMAIL_ADRESS_2}`, // sender address
+    to: process.env.EMAIL_ADRESS_2, // list of receivers
     subject: "smartEdu contact New Message ✔", // Subject line
     html: outputMessage, // html body
   });
 
   console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  // Message sent: 
 
   // Preview only available when sending through an Ethereal account
   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  // Preview URL: 
   
   req.flash("success", "We Received your message succesfully");
   res.status(200).redirect("contact");
